c15f84141e34efd40f14d8a747616aa724454c29
Mrigangka Datta <mrigangkadatta15@gmail.com>
2025-02-22 17:09:43 +0530

    Add data modules for features, testimonials, and how it works; update Home page to display features

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 import HeroSection from "@/components/Hero";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data";
+import { title } from "process";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <section>
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
         <div>
           <h2>Powerful Features for Your Career Growth</h2>
+          <div>
+            {
+              features.map((feature,index)=>{
+                return (
+                  <Card>
+                    <CardContent>
+                      <div key={title}>{feature.icon}
+                        <h3>{feature.title}</h3>
+                        <p>{feature.description}</p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                )
+              })
+            }
+          </div>
         </div>
       </section>
     </>
---
+++ data/index.ts
@@ -0,0 +1,13 @@
+import { faqs } from "./faqs";
+import {features} from "./features"
+import { howItWorks } from "./howItWorks";
+import { industries } from "./industries";
+import { testimonial } from "./testimonial";
+
+export{
+    faqs,
+    features,
+    howItWorks,
+    industries,
+    testimonial
+}
