426411aeefab67a413307928e0e4534e58f86fb8
Mrigangka Datta <mrigangkadatta15@gmail.com>
2025-02-25 22:02:56 +0530

    Add Industry Insights page and custom 404 Not Found component

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndustryInsightsPage = () => {
+    return (
+        <div>
+            <h1>Industry Insights</h1>
+            <p>Welcome to the Industry Insights page. Here you will find the latest trends and analysis in the industry.</p>
+        </div>
+    );
+};
+
+export default IndustryInsightsPage;
