6485f8a043fb8aad6c3150933f146f9dae05c23d
Mrigangka Datta <mrigangkadatta15@gmail.com>
2025-03-09 00:06:38 +0530

    Implement user onboarding checks and remove unused interview pages; add onboarding form component

---
+++ app/(main)/onboarding/_components/onboarding-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const OnboardingForm = ({industries}:any) => {
+  return (
+    <div>onboarding-form</div>
+  )
+}
+
+export default OnboardingForm
--- app/(main)/onboarding/page.tsx
+++ app/(main)/onboarding/page.tsx
@@ -1,10 +1,23 @@
+import { industries } from '@/data'
 import React from 'react'
+import OnboardingForm from './_components/onboarding-form'
+import { getUserOnboardingStatus } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 type Props = {}
 
-const onboardingPage = (props: Props) => {
+const onboardingPage = async(props: Props) => {
+
+  const {isOnboarded}= await getUserOnboardingStatus();
+
+  if(isOnboarded)
+    redirect("/dashboard")
+
+
   return (
-    <div>onboardingPage</div>
+    <main>
+      <OnboardingForm industries = {industries}/>
+    </main>
   )
 }
 
